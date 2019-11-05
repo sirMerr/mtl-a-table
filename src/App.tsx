881b71ba85f84d74f87666c1d132fca9a202b160
Tiffany Le-Nguyen <tlenguyen@expedia.com>
2019-11-05 18:31:26 -0500

    feat: make it pretty by adding material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import { getAllBrunchRestaurants } from './api';
 import brunchRestaurants from './data/brunch-restaurants.json';
+import RestaurantCard from './components/card';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
 
 // Right now, just brunch restaurants
 const App: React.FC = () => {
+  const classes = useStyles();
+
   // useEffect(() => {
   //   async function fetchRestaurants() {
   //     const restaurants = await getAllBrunchRestaurants();
@@ -18,17 +38,25 @@ const App: React.FC = () => {
   // console.log(brunchRestaurants);
 
   return (
-    <div className="App">
-      <ul>
+    <Container>
+      <Typography variant="h1" component="h2" gutterBottom>
+        MTL a Table Brunch Restaurants
+      </Typography>
+      <Grid container className={classes.root} spacing={2}>
         {brunchRestaurants.map(restaurant => {
           return (
-            <li key={restaurant.data_nid}>
-              <a href={`https://mtlatable.mtl.org/en/${restaurant.url}`}>{restaurant.title}</a>
-            </li>
+            <Grid item xs={3} key={restaurant.data_nid}>
+              <RestaurantCard
+                image={restaurant.background_image_url}
+                title={restaurant.title}
+                description={restaurant.summary || ""}
+                link={`https://mtlatable.mtl.org/en/${restaurant.url}`}
+              />
+            </Grid>
           );
         })}
-      </ul>
-    </div>
+      </Grid>
+    </Container>
   );
 }
 
